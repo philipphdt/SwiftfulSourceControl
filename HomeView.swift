ac287cdeacc4b98cbdc6ac0dc45c3178d2b2755d
Philipp Hdt <philipp.hdt@hotmail.de>
2024-05-18 14:45:09 +0200

    Add HomeView

---
+++ HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Philipp Haßdenteufel on 18.05.24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
