7597a7ea7e123f00fa511e2153972537b80a5b99
Philipp Hdt <philipp.hdt@hotmail.de>
2024-05-18 15:28:28 +0200

    [Feature] Add title to HomeView

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var title: String = ""
+    
     var body: some View {
         VStack {
             Text("Screen 2")
             
             Text("Screen 2")
-            Text("Screen 2")
+            Text("Screen 3")
         }
     }
 }
