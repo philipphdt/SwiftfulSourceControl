7cea85a3e803fb74028838425a7d877aef49a738
Philipp Hdt <philipp.hdt@hotmail.de>
2024-05-19 15:38:11 +0200

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Philipp Haßdenteufel on 19.05.24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
